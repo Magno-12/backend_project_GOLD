92ad53d3cdde59bf0bbce3d802dbc242f50ac47a
Magno Martinez <mmartinezbu@ia.com.mx>
2024-12-07 14:59:17 -0500

    [feat]: nueva estructura

--- app/models/blacklisted_token.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, String, DateTime
-from app.db.base_class import Base
-
-
-class BlacklistedToken(Base):
-    __tablename__ = "blacklisted_tokens"
-
-    id = Column(String, primary_key=True, index=True)
-    blacklisted_on = Column(DateTime, nullable=False)
