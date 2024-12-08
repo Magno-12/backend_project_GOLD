7b77a456f354206eda7c36f5f25f480ffc249fa1
Magno Martinez <mmartinezbu@ia.com.mx>
2024-12-08 13:53:32 -0500

    [fix]: ajuste de ruta

--- backend_GOLD/urls.py
+++ backend_GOLD/urls.py
@@ -38,11 +38,12 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+
     # API URLs
     path('api/users/', include('apps.users.urls')),
     path('api/auth/', include('apps.authentication.urls')),
-    
+    path('api/lottery/', include('apps.lottery.urls')),
+
     # Swagger URLs
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
