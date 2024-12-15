f62a69d5e64cd47565aa266bfc0045d74672aaa9
Magno Martinez <mmartinezbu@ia.com.mx>
2024-12-15 17:21:41 -0500

    [fix]: db

--- backend_GOLD/settings.py
+++ backend_GOLD/settings.py
@@ -113,27 +113,27 @@ WSGI_APPLICATION = 'backend_GOLD.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
-}
-#DATABASE-TEST
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres.skjbvqoleimipifehmae',
-#         'PASSWORD': '5C3nxg:n7bp3Y2K',
-#         'HOST': 'aws-0-us-west-1.pooler.supabase.com',
-#         'PORT': 6543,
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
 #     }
 # }
+#DATABASE-TEST
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.skjbvqoleimipifehmae',
+        'PASSWORD': '5C3nxg:n7bp3Y2K',
+        'HOST': 'aws-0-us-west-1.pooler.supabase.com',
+        'PORT': 6543,
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
