e3241bca3bd277cdcf97ca2479fb8ebbcf26a612
Magno Martinez <mmartinezbu@ia.com.mx>
2025-03-02 22:43:41 -0500

    import

--- apps/lottery/views/lottery_view.py
+++ apps/lottery/views/lottery_view.py
@@ -9,7 +9,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.utils import timezone
-from django.db import transaction
+from django.db import transaction, models
 from decimal import Decimal
 
 from apps.lottery.models import Lottery, LotteryResult, Bet
